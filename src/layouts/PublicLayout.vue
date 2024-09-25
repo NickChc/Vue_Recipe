b87fb6912ed7d84ff3e9d707b7626851a08d188d
unknown <nika.chichua26@gmail.com>
2024-09-25 21:46:23 +0400

    feat: safari scroll prevented

--- src/layouts/PublicLayout.vue
+++ src/layouts/PublicLayout.vue
@@ -12,9 +12,16 @@ const { sideBarOpen } = storeToRefs(globalStore);
 <template>
   <SideBar />
   <div
-    :class="`duration-[400ms] transition-transform sm:transition-none ease-in h-full overflow-hidden ${
+    @touchmove="
+      (e) => {
+        if (sideBarOpen) {
+          e.preventDefault();
+        }
+      }
+    "
+    :class="`duration-[400ms] transition-transform sm:transition-none ease-in h-full overflow-hidden touch-none ${
       sideBarOpen
-        ? `translate-x-[calc(100%-var(--width-sidebar-btn))] sm:translate-x-0`
+        ? `translate-x-[calc(100%-var(--width-sidebar-btn))] sm:translate-x-0 touch-none overflow-hidden`
         : 'translate-x-0'
     }`"
   >
